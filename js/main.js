fd42a253ffecdbd77cb63215898bfef2f39277b8
Stephanie Corrales <stephaniecorrales@Stephanies-MacBook-Pro-2.local>
2018-12-03 08:37:55 -0800

    updated styling, links, video

--- js/main.js
+++ js/main.js
@@ -1,29 +1,45 @@
-$('#content-1').click(
-	function() {
-		$('#moreinfo1').slideToggle("normal");
-	}
-);
+// // 1
 
-$('#content-2').click(
-	function() {
-		$('#moreinfo2').slideToggle("normal");
-	}
-);
+// $('#content-1').click(
+// 	function() {
+// 		$('#moreinfo1').slideToggle("normal");
+// // // 		$('.arrow1').toggleClass('active');
+// });
 
-$('#content-3').click(
-	function() {
-		$('#moreinfo3').slideToggle("slow");
-	}
-);
+// // // 2
+// $('#content-2').click(
+// 	function() {
+// 		$('#moreinfo2').slideToggle("normal");
+// // 		$('.arrow2').toggleClass('active');
+// });
 
-$('#content-4').click(
+// // // 3
+// $('#content-3').click(
+// 	function() {
+// 		$('#moreinfo3').slideToggle();
+// // 		$('.arrow3').toggleClass('active');
+// });
+
+// // // 4
+// $('#content-4').click(
+// 	function() {
+// 		$('#moreinfo4').slideToggle();
+// // 		$('.arrow4').toggleClass('active');
+// });
+
+// // // 5
+// $('#content-5').click(
+// 	function() {
+// 		$('#moreinfo5').slideToggle();
+// // 		$('.arrow5').toggleClass('active');
+// });
+
+$('.arrow1').click(
 	function() {
+		$('.arrow1').toggleClass('active');
+		$('#moreinfo1').slideToggle();
+		$('#moreinfo2').slideToggle();
+		$('#moreinfo3').slideToggle();
 		$('#moreinfo4').slideToggle();
-	}
-);
-
-$('#content-5').click(
-	function() {
 		$('#moreinfo5').slideToggle();
-	}
-);
+});
