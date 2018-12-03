95849cd0338e160788afa714686a0878317d9b81
Stephanie Corrales <stephaniecorrales@Stephanies-MacBook-Pro-2.local>
2018-12-02 19:45:36 -0800

    Slide toggle fix yay!

---
+++ js/main.js
@@ -0,0 +1,29 @@
+$('#content-1').click(
+	function() {
+		$('#moreinfo1').slideToggle("normal");
+	}
+);
+
+$('#content-2').click(
+	function() {
+		$('#moreinfo2').slideToggle("normal");
+	}
+);
+
+$('#content-3').click(
+	function() {
+		$('#moreinfo3').slideToggle("slow");
+	}
+);
+
+$('#content-4').click(
+	function() {
+		$('#moreinfo4').slideToggle();
+	}
+);
+
+$('#content-5').click(
+	function() {
+		$('#moreinfo5').slideToggle();
+	}
+);
